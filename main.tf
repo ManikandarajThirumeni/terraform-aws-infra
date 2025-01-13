df4a16bfe502b451c9e417afe530e090c63e30ca
rajarajank23 <rajarajank23@gmail.com>
2025-01-13 23:00:40 +0530

    Update main.tf
    yes changed code

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 #tfsec:ignore:aws-s3-enable-bucket-logging
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-example-bucket-999"
@@ -52,4 +48,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
       sse_algorithm     = "aws:kms"
     }
   }
-}
